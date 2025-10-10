b220bd11299392dff208725f75ea20584306628f
WADPH <75982625+WADPH@users.noreply.github.com>
2025-10-10 15:29:28 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Uncomment this If you have requierement.txt
+#pip install --no-cache-dir -r requirements.txt 
 streamlit run /app/app.py --server.port=8502 --server.address=0.0.0.0 &
 nginx -g 'daemon off;' &
 cloudflared tunnel --no-autoupdate run --token Your_CloudflareTunnel_Token &
